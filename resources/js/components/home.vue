476aca96d9a2ad1d9bc9fd36d27a47ff52aea48e
Whylgher <7bitsmarket@gmail.com>
2023-03-18 15:57:45 +0000

    add home and index

---
+++ resources/js/components/home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row mb-md-5 mb-4 mx-n2">
+            <div class="col-lg-3 col-6 product-block mb-4 px-2">
+
+                <a href="">
+                    <img class="img-fluid img-portfolio img-hover mb-3"
+                        src="https://dt7v1i9vyp3mf.cloudfront.net/styles/news_large/s3/imagelibrary/a/applenotes02-0708-N5XC8GqolB4ZiZtz.dDOayRMDD7SOljK.jpg"
+                        alt="iMac Desktop Computer">
+                </a>
+
+                <div class="caption">
+                    <h3 class="page-header">iMac Desktop Computer</h3>
+                    <div class="price-mob mb-2">
+                        $100.000
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Products',
+    data() {
+    },
+    mounted() {
+    },
+    methods: {
+    }
+}
+</script>
